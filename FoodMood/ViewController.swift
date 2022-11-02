d8b46226815eca180008f00985f8962a5d3d4be2
Kaitlyn Squyres <ksquyres@fordham.edu>
2022-11-02 15:23:13 -0400

    testing commits

--- FoodMood/ViewController.swift
+++ FoodMood/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("Hi!")
         //testing123
+        print("Hello")
+
 
     }
 
