d5755b5966fc614b32ba504728311b4c9a036a61
jgarvey12 <jgarvey12@fordham.edu>
2022-11-02 15:39:03 -0400

    testing from command line

--- FoodMood/ViewController.swift
+++ FoodMood/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("Hi!")
         //testing123
-
+        //test again 123
     }
 
 
